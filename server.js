569a33523964ecb785263eff110ce20a617adaf8
ug02fast <arthurzhuk@gmail.com>
2016-02-08 18:23:21 -0800

    fuck me

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ var path = require('path');
 var webpack = require('webpack');
 var app = express();
 
-var isDevelopment = (process.env.NODE_ENV !== 'production');
 var static_path = path.join(__dirname, './');
 
-app.use(express.static(static_path))
+app.use(express.static(__dirname))
   .get('/', function (req, res) {
     res.sendFile('index.html', {
-      root: static_path
+      root: __dirname
     });
   }).listen(process.env.PORT || 8080, function (err) {
     if (err) { console.log(err) };
